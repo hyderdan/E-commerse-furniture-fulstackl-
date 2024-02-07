3f443f5b5a90caebc1d2848c30f9b0be6431f220
Hyder Danish <hyder.danish369@gmail.com>
2024-02-07 18:04:08 +0530

    workdone

--- client/src/Components/Payment.jsx
+++ client/src/Components/Payment.jsx
@@ -8,9 +8,11 @@ import Getid from "./session";
 export default function Payment(){
     const {Cartproducts}=useContext(mydata);
     const[order,setorder]=useState([]);
+    const [Totalprice,settotalprice]=useState(0)
     const sessionid = Getid();
     useEffect(()=>{
         fetchcart();
+        
     },[])
     const fetchcart = async () => {
         try {
@@ -19,23 +21,46 @@ export default function Payment(){
           const response = await axios.get(`http://localhost:5000/users/savedcart/${sessionid}`, {})
           setorder(response.data.products);
           console.log("onlyidincart", response.data.products);
-         
+          
+          settotalprice(calculateTotalPrice ());
+          console.log("O",Totalprice)
         }
         catch (err) {
           console.log(err);
         }
       }
+      const calculateTotalPrice = () => {
+        let totalPrice = 0;
     
+        order.forEach((data) => {
+          totalPrice += data.product.price * data.quantity
+        });
+        return totalPrice;
+        
+      };
+     
     return(
        <section className="Sectionp">
         <div className="Pcontainer">
         <h1>Payment Details</h1>
-        </div>
+        
         <div className="orders">
-           {order.map((data)=>(
-            
-           ))}
         </div>
+        <div className="orderdetails">
+          
+          </div>
+          <div className="amountpay">
+        <h5>Amount Payable</h5><span className="totalmoney">₹{calculateTotalPrice()}</span>
+        <div className="amountsub">
+        {order.map((data)=>(
+          <div>
+          <h6>{data.product.name}</h6>
+          <p className="price">₹{data.product.price}</p> 
+          </div>
+        ))}
+        </div>
+        </div>
+          </div>
        </section>
     )
 }
